e81e78b3747888449428ac23e8414e2dbfe6044d
andche742 <andche742@gmail.com>
2025-05-02 18:19:25 -0700

    finish data cleaning

---
+++ data_processing/scripts/normalization.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+input_file="./data/normalized.csv"
+output_file="./data/temp1.csv"
+nscore=3
+ss=10
+
+awk -F"," -v first_c="$nscore" -v last_c="$ss" 'BEGIN {OFS = ","}
+  NR == 1 {
+    header = $0;
+    next;
+  }
+
+  {
+    row[NR] = $0;
+    split($0, cols, ",");
+    row_count[NR] = length(cols);  # Save NF
+    for (col = first_c; col <= last_c; col++) {
+      val = cols[col] + 0;
+      if (!(col in min) || val < min[col]) min[col] = val;
+      if (!(col in max) || val > max[col]) max[col] = val;
+    }
+  }
+
+  END {
+    print header;
+
+    for (i = 2; i <= NR; i++) {
+      split(row[i], cols, ",");
+      for (col = first_c; col <= last_c; col++) {
+        val = cols[col] + 0;
+        if (min[col] == max[col]) {
+          cols[col] = 0;
+        } else {
+          cols[col] = (val - min[col]) / (max[col] - min[col]);
+        }
+      }
+
+      for (j = 1; j <= row_count[i]; j++) {
+        printf("%s", cols[j]);
+        if (j < row_count[i]) printf(",");
+      }
+      print "";
+    }
+  }
+' "$input_file" > "$output_file"
